9f30957ac2b26aee26aafe3a3e2b8222d27c41c8
Anna Topilina <anna-topilina@mail.ru>
2021-11-15 17:50:33 +0300

    build project

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 import '../scss/style.scss';
 
 console.log('Works!');
+
+var buttonShowAllText = document.querySelector('.blog-more');
+var text = document.querySelector('.blog__text');
+
+
+buttonShowAllText.addEventListener('click', function (event) {
+  buttonShowAllText.classList.remove('blog-more')
+  buttonShowAllText.classList.add('blog-more--state--active');
+  // text.classList.remove('blog__text')
+  text.classList.add('blog__text--state--active')
+})
