9529f7f8a8b9fc92b12b65db8a9181a5725ceb76
Anna Topilina <anna-topilina@mail.ru>
2021-11-16 12:43:45 +0300

    create swiper-brands

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
 import '../scss/style.scss';
 
-console.log('Works!');
+const swiper = new Swiper('.swiper-container', {
 
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  spaceBetween: 16,
+  slidesPerView: 1.15,
+  centeredSlides: true,
+
+});
+
+// Кнока Показать все у текста в статье
 var buttonShowAllText = document.querySelector('.blog-more');
 var text = document.querySelector('.blog__text');
 
@@ -11,4 +23,21 @@ buttonShowAllText.addEventListener('click', function (event) {
   buttonShowAllText.classList.add('blog-more--state--active');
   // text.classList.remove('blog__text')
   text.classList.add('blog__text--state--active')
+})
+
+// Показать все/Скрыть у брендов
+var buttonShowAllButtons = document.querySelector('.brands-more');
+var buttonHideAllButtons = document.querySelector('.brands-hide');
+var brands = document.querySelector('.brands-main');
+
+
+buttonShowAllButtons.addEventListener('click', function (event) {
+  buttonShowAllButtons.classList.add('brands-more--state--active');
+  buttonHideAllButtons.classList.remove('brands-hide--state--active');
+  brands.classList.add('brands-main--show')
+})
+buttonHideAllButtons.addEventListener('click', function (event) {
+  buttonHideAllButtons.classList.add('brands-hide--state--active');
+  buttonShowAllButtons.classList.remove('brands-more--state--active');
+  brands.classList.remove('brands-main--show');
 })
